17fd3bb9207ba3479ae8735cf8d323521065f88d
Akarsh_M_M <94667880+akarshmm@users.noreply.github.com>
2023-01-13 12:36:51 +0530

    Add files via upload

---
+++ pianno/js/script1.js
@@ -0,0 +1,49 @@
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 65) {
+    document.getElementById("track1").play();
+  } else if (e.keyCode == 66) {
+    document.getElementById("track2").play();
+  } else if (e.keyCode == 67) {
+    document.getElementById("track3").play();
+  } else if (e.keyCode == 68) {
+    document.getElementById("track4").play();
+  } else if (e.keyCode == 69) {
+    document.getElementById("track5").play();
+  } else if (e.keyCode == 70) {
+    document.getElementById("track6").play();
+  } else if (e.keyCode == 71) {
+    document.getElementById("track7").play();
+  } else if (e.keyCode == 72) {
+    document.getElementById("track8").play();
+  } else if (e.keyCode == 73) {
+    document.getElementById("track9").play();
+  } else if (e.keyCode == 74) {
+    document.getElementById("track10").play();
+  } else if (e.keyCode == 75) {
+    document.getElementById("track11").play();
+  } else if (e.keyCode == 76) {
+    document.getElementById("track12").play();
+  } else if (e.keyCode == 77) {
+    document.getElementById("track13").play();
+  } else if (e.keyCode == 78) {
+    document.getElementById("track14").play();
+  } else if (e.keyCode == 79) {
+    document.getElementById("track15").play();
+  } else if (e.keyCode == 80) {
+    document.getElementById("track16").play();
+  } else if (e.keyCode == 81) {
+    document.getElementById("track17").play();
+  } else if (e.keyCode == 82) {
+    document.getElementById("track18").play();
+  } else if (e.keyCode == 83) {
+    document.getElementById("track19").play();
+  } else if (e.keyCode == 84) {
+    document.getElementById("track20").play();
+  } else if (e.keyCode == 85) {
+    document.getElementById("track21").play();
+  } else if (e.keyCode == 86) {
+    document.getElementById("track22").play();
+  } else if (e.keyCode == 87) {
+    document.getElementById("track23").play();
+  }
+});
